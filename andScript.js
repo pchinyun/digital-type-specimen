08932f0c4780bc1aa1246655dcdff1932345c5fd
CY <pchinyun@gmail.com>
2025-08-19 18:48:43 +1000

    moved three.modules

--- andScript.js
+++ andScript.js
@@ -1,4 +1,4 @@
-import * as THREE from "./node_modules/three/build/three.module.js"
+import * as THREE from './three.module'
 import './style.css'; 
 import { OrbitControls } from './OrbitControls.js';
 import { FontLoader } from './FontLoader.js'; 
