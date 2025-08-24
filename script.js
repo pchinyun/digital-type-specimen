eb0d662ebeb2c0ebcfad875383d5fa91f9cae360
CY <pchinyun@gmail.com>
2025-08-24 21:04:56 +1000

    Remove leftover gh-pages cache

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as THREE from './three.module.js'
+import * as THREE from './build/three.module'
 import './style.css'; 
 import { OrbitControls } from './OrbitControls.js';
 import { FontLoader } from './FontLoader.js'; 
